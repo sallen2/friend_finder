e1681a5f364419bad5d01890a831f8544d90a5af
Stephan Allen <sallen202009@gmail.com>
2018-11-13 16:55:46 -0500

    finished logic for friend finder

--- app/data/friends.js
+++ app/data/friends.js
@@ -10,44 +10,91 @@ const pool = mysql.createPool({
 });
 
 let dbServer = {
-  selectFrom: (val, res) => {
-    let a;
-    pool.getConnection((err, con) => {
-      if (err) throw err;
-      con.query(`SELECT ${val} FROM people`, (err, resp) => {
+  selectFrom: val => {
+    return new Promise((resolve, reject) => {
+      pool.getConnection((err, con) => {
         if (err) throw err;
-        res.json(resp);
-      });
-    });
-  },
-  createTable: (name, url, scores) => {
-    pool.getConnection((err, con) => {
-      con.query(
-        `INSERT INTO people SET ?`,
-        {
-          person_name: name,
-          photo: url,
-          scores: scores
-        },
-        (err, resp) => {
-          if (err) throw err;
-          console.log("column set!");
-        }
-      );
-    });
-  },
-  findFriends: res => {
-    pool.getConnection((err, con) => {
-      con.query("SELECT * FROM people", (err, resp) => {
-        resp.forEach(person => {
-          let a = person.scores.replace(/'+/g, "").split(",");
-          console.log(a)
+        con.query(`SELECT ${val} FROM people`, (err, resp) => {
+          if (err) reject(err);
+          resolve(resp);
         });
       });
     });
   },
-  endConnnect: () => {
-    con.end();
+  selectWhere: user => {
+    return new Promise((resolve, reject) => {
+      pool.getConnection((err, con) => {
+        if (err) throw err;
+        con.query(
+          "SELECT * FROM people WHERE ?",
+          { person_name: user },
+          (err, resp) => {
+            if (err) reject(err);
+            resolve(resp);
+          }
+        );
+      });
+    });
+  },
+  createColumn: (name, url, scores) => {
+    return new Promise((resolve, reject) => {
+      pool.getConnection((err, con) => {
+        con.query(
+          `INSERT INTO people SET ?`,
+          {
+            person_name: name,
+            photoUrl: url,
+            scores: scores
+          },
+          (err, resp) => {
+            if (err) reject(err);
+            resolve("column set!");
+          }
+        );
+      });
+    });
+  },
+  sendCompatibleFriends: user => {
+    return new Promise((resolve, reject) => {
+      dbServer.selectWhere(user).then(res => {
+        dbServer
+          .findFriends(res)
+          .then(resp => {
+            resolve(resp);
+          })
+          .catch(err => {
+            if (err) throw err;
+          });
+      });
+    });
+  },
+  findFriends: user => {
+    return new Promise((resolve, reject) => {
+      let client = user[0]
+      dbServer.selectFrom('*')
+      .then(res=>{
+        let arr = []
+        res.forEach(person=>{
+          if(client.person_name === person.person_name){
+            // 
+          }else{
+            let num = 0;
+            let cUser = client.scores.replace(/'+/g,"").split(",")
+            let pFriend = person.scores.replace(/'+/g,"").split(",")
+            for(let i = 0; i < pFriend.length; i++){
+              if(cUser[i] === pFriend[i]){
+                num++
+                if(num >= 3){
+                  arr.push(person)
+                  break;
+                }
+              }
+            }
+          }
+          resolve(arr)
+        })
+      })
+    });
   }
 };
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,20 +5,43 @@ const router = express.Router();
 
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
+// global variables
+let name = "undefined";
 
 router.get("/friends", (req, res) => {
-  setTimeout(()=>{
-    friendsdb.selectFrom("*", res);
-    friendsdb.findFriends(res);
-  },1500)
+  if (name === "undefined") {
+    res.redirect('/survey');
+  } else {
+    friendsdb.sendCompatibleFriends(name).then(friends => {
+      res.json(friends);
+    });
+  }
 });
 
 router.post("/friends", (req, res) => {
-  let name = req.body.name;
+  name = req.body.name;
   let url = req.body.photo;
-  let score = [req.body.q1,req.body.q2,req.body.q3,req.body.q4,req.body.q5,req.body.q6,req.body.q7,req.body.q8,req.body.q9,req.body.q10]
-  friendsdb.createTable(name, url, `${score}`);
-  res.redirect("/api/friends");
+  let score = [
+    req.body.q1,
+    req.body.q2,
+    req.body.q3,
+    req.body.q4,
+    req.body.q5,
+    req.body.q6,
+    req.body.q7,
+    req.body.q8,
+    req.body.q9,
+    req.body.q10
+  ];
+  friendsdb
+    .createColumn(name, url, `${score}`)
+    .then(resp => {
+      console.log(resp);
+      res.redirect("/api/friends");
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
